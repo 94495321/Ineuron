46f69cd1bfbe39d0e9a1f989f675f9f6e5f0de2d
94495321 <77776430+94495321@users.noreply.github.com>
2022-10-26 12:13:00 +0530

    Add files via upload

---
+++ assignment 1/Number1234.java
@@ -0,0 +1,21 @@
+package loops.main;
+
+public class Number1234 {
+	
+	public static void main(String [] args)
+	{
+		int m=4;
+		int n=5;
+		for(int i=1;i<n; i++)
+		{
+			for(int j=0;j<m;j++)
+			{
+				
+					System.out.print(i);
+				
+			}
+			System.out.println();	
+		}
+	}
+
+}
